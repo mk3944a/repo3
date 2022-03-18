bfef5e26be5f77091a8c0d65dc8ad70dfacbb3b2
mk3944a <78238587+mk3944a@users.noreply.github.com>
2022-03-18 05:01:35 +0500

    Update DS413613ClassworkLabForkingRfile.R

--- DS413613ClassworkLabForkingRfile.R
+++ DS413613ClassworkLabForkingRfile.R
@@ -3,7 +3,7 @@ library(ggplot2)
 library(dplyr)
 mpg
 # My name is Rey Kazi
-# Thw data source is the mpg data table
+# The data source is the mpg data table
 # This is my coding that generates Boxplots for transmission types
 ggplot(data = mpg) +
   geom_boxplot(mapping = aes(x = drv, y = displ, fill = drv)) +
